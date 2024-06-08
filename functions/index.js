4310b9ec293320d6915e17c79b4e80589fcd5f2a
ultramolecular <josiah.py@gmail.com>
2024-06-08 00:56:46 -0500

    Fixed linting errors on functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,16 @@
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const { exec } = require("child_process");
 const path = require("path");
 
 exports.app = onRequest((req, res) => {
-    const scriptPath = path.resolve(__dirname, "../backend/main.py");
-    
-    exec(`python ${scriptPath}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error}`);
-            res.status(500).send(stderr);
-        } else {
-            res.status(200).send(stdout);
-        }
-    });
+  const scriptPath = path.resolve(__dirname, "../backend/main.py");
+
+  exec(`python ${scriptPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+      res.status(500).send(stderr);
+    } else {
+      res.status(200).send(stdout);
+    }
+  });
 });
